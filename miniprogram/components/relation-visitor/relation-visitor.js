1ed817e38c8cc54060aa34ed2ae77eb86bf84c7e
damoqiongqiu <damoqiongqiu@126.com>
2019-09-11 10:33:15 +0800

    列表页改用scroll-view

--- miniprogram/components/relation-visitor/relation-visitor.js
+++ miniprogram/components/relation-visitor/relation-visitor.js
@@ -3,9 +3,24 @@ Component({
         userId:String
     },
     data:{
+        loading:true
     },
     methods:{
+        loadData(){
+            let that=this;
+            console.log("正在加载数据...");
+            setTimeout(()=>{
+                that.setData({
+                    loading:false
+                });
+            },2000);
+        },
+        loadMore(){
+            console.log("下拉加载更多...");
+            this.loadData();
+        }
     },
     ready() {
+        this.loadData();
     }
 })
