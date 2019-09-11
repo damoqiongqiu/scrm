1695a169810ba31b270b94466131e032b691fa93
damoqiongqiu <damoqiongqiu@126.com>
2019-09-11 11:14:02 +0800

    修改图文详情界面

--- miniprogram/pages/edit-detail-images/edit-detail-images.js
+++ miniprogram/pages/edit-detail-images/edit-detail-images.js
@@ -1,5 +1,6 @@
 Page({
     data: {
+        images: []
     },
     onLoad: function () {
         
@@ -8,5 +9,15 @@ Page({
         wx.navigateBack({
             delta: 1
         });
+    },
+    chooseImage(e) {
+        wx.chooseImage({
+            sizeType: ['original', 'compressed'],  //可选择原图或压缩后的图片
+            sourceType: ['album', 'camera'], //可选择性开放访问相册、相机
+            success: res => {
+                const images = this.data.images.concat(res.tempFilePaths);
+                this.data.images = images.length <= 3 ? images : images.slice(0, 3);
+            }
+        })
     }
 })
