cd5843663986da7bc05225ab30f4edeeebf70d0b
damoqiongqiu <damoqiongqiu@126.com>
2019-09-11 11:01:18 +0800

    选择名片模板界面修改

--- miniprogram/pages/choose-card-template/choose-card-template.js
+++ miniprogram/pages/choose-card-template/choose-card-template.js
@@ -1,8 +1,22 @@
 Page({
-    data: {
+    data:{
+        loading:true
     },
     onLoad: function () {
-        
+        this.loadData();
+    },
+    loadData(){
+        let that=this;
+        console.log("正在加载数据...");
+        setTimeout(()=>{
+            that.setData({
+                loading:false
+            });
+        },2000);
+    },
+    loadMore(){
+        console.log("下拉加载更多...");
+        this.loadData();
     },
     toEditMain(e){
         wx.navigateBack({
