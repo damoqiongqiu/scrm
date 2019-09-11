36750ec5d77ddae54ca279971fff794aca27f35c
damoqiongqiu <damoqiongqiu@126.com>
2019-09-11 14:09:04 +0800

    加V验证

--- miniprogram/pages/verify/verify.js
+++ miniprogram/pages/verify/verify.js
@@ -1,11 +1,46 @@
 Page({
-    data: {    },
+    data: {  
+        imgSrc:""
+    },
     onLoad: function () {
         
     },
-    toEditMain(e){
-        wx.navigateBack({
-            delta: 1
-        });
+    chooseImg(e){
+        let that=this;
+        wx.showActionSheet({
+            itemList: ['拍摄', '从相册选择'],
+            success (res) {
+                if(res.tapIndex==0){
+                    wx.chooseImage({
+                        count: 1,
+                        sizeType: ['original', 'compressed'],
+                        sourceType: ['camera'],
+                        success (res) {
+                            const tempFilePaths = res.tempFilePaths
+                            console.log(tempFilePaths);
+                            that.setData({
+                                imgSrc:tempFilePaths
+                            });
+                        }
+                    });
+                }else if(res.tapIndex==1){
+                    wx.chooseImage({
+                        count: 1,
+                        sizeType: ['original', 'compressed'],
+                        sourceType: ['album'],
+                        success (res) {
+                            const tempFilePaths = res.tempFilePaths
+                            console.log(tempFilePaths);
+                            that.setData({
+                                imgSrc:tempFilePaths
+                            });
+                        }
+                    });
+                }
+            },
+            fail (res) {
+                console.log(res.errMsg)
+            }
+        })
     }
 })
