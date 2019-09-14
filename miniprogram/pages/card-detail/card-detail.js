f1b7d59a38e8f258ae9d8b78e6c22b1e98c958e9
damoqiongqiu <damoqiongqiu@126.com>
2019-09-14 20:41:22 +0800

    图文详情与后端对接

--- miniprogram/pages/card-detail/card-detail.js
+++ miniprogram/pages/card-detail/card-detail.js
@@ -2,12 +2,36 @@ const app = getApp();
 
 Page({
     data: {
-        userId:""
+        userId:"",
+        cardDetails:{}
+    },
+    loadCardDetails:function(){
+        let that=this;
+            const db=wx.cloud.database();
+            const _=db.command;
+            db.collection("card-details")
+                .where({
+                    userId:this.data.userId
+                })
+                .limit(1)
+                .get({
+                    success:function(res){
+                        if(res&&res.data&&res.data.length){
+                            that.setData({
+                                cardDetails:res.data[0]
+                            });
+                        }
+                    },
+                    fail:function(event){
+                        console.error(event);
+                    }
+                });
     },
     onLoad: function () {
         this.setData({
             userId:app.globalData.userInfo._id
         });
+        this.loadCardDetails();
     },
     toEdit(e){
         wx.navigateTo({
